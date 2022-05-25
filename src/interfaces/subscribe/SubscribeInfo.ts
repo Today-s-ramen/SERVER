5b831a0fb7845d4fea4ad9ac13a07d7473ceff17
Youjung Heo <hujumee27@gmail.com>
2022-05-25 19:22:55 +0900

    [CHORE#1] subscribe collection schema 및 interface 수정

--- src/interfaces/subscribe/SubscribeInfo.ts
+++ src/interfaces/subscribe/SubscribeInfo.ts
@@ -2,7 +2,6 @@ import { PeriodInfo } from "../period/PeriodInfo";
 import { QuantityInfo } from "../quantity/QuantityInfo";
 
 export interface SubscribeInfo {
-    packageName: String;
     period: PeriodInfo;
     quantity: QuantityInfo;
 };
--- src/models/Subscribe.ts
+++ src/models/Subscribe.ts
@@ -2,10 +2,6 @@ import mongoose from "mongoose";
 import { SubscribeInfo } from "../interfaces/subscribe/SubscribeInfo";
 
 const SubscribeSchema = new mongoose.Schema ({
-    packageName: {
-        type: String,
-        required: true
-    },
     period: {
         option: {
             type: String,
