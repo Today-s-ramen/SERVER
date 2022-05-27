b912c144ebdb1642aba5cba61c51a601c521c927
Youjung Heo <hujumee27@gmail.com>
2022-05-27 17:23:49 +0900

    [FEAT: #8] 구독 등록 기능 구현

--- src/controllers/SubscribeController.ts
+++ src/controllers/SubscribeController.ts
@@ -3,6 +3,9 @@ import message from '../modules/responseMessage';
 import statusCode from '../modules/statusCode';
 import util from '../modules/util';
 import SubscribeService from '../services/SubscribeService';
+import { SubscribeCreateDto } from '../interfaces/subscribe/SubscribeCreateDto';
+import { rmSync } from 'fs';
+const { validationResult } = require('express-validator');
 
 const getSubscribeOptions = async (req: Request, res: Response) => {
   try {
@@ -17,4 +20,26 @@ const getSubscribeOptions = async (req: Request, res: Response) => {
   }
 };
 
-export default { getSubscribeOptions };
+/**
+ * @route POST /subscribe/
+ * @desc create subscribe
+ */
+const createSubscribe = async (req: Request, res: Response) => {
+  const error = validationResult(req);
+  if(!error.isEmpty()) {
+    res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, message.BAD_REQUEST));
+  }
+
+  const SubscribeCreateDto: SubscribeCreateDto = req.body;
+
+  try {
+    const data = await SubscribeService.createSubscribe(SubscribeCreateDto);
+
+    res.status(statusCode.OK).send(util.success(statusCode.OK, message.CREATE_SUBSCRIBE_SUCCESS, data));
+  } catch(error) {
+    console.log(error);
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+  }
+}
+
+export default { getSubscribeOptions, createSubscribe };
--- src/routes/SubscribeRouter.ts
+++ src/routes/SubscribeRouter.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import { SubscribeController } from '../controllers';
+import { body } from 'express-validator';
 
 const router: Router = Router();
 
 router.get('/', SubscribeController.getSubscribeOptions);
+
+router.post('/', [
+    body('deliveryPeriod').notEmpty(),
+    body('deliveryQuantity').notEmpty()
+], SubscribeController.createSubscribe);
 
 export default router;
